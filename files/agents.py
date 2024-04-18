33aae046bb4d08791c0eb59d8dee33c80d2eae05
Alex_desktop <alexandre-1.hebert@outlook.com>
2024-04-18 08:42:16 -0400

    Changed structure of files

---
+++ files/agents.py
@@ -0,0 +1,40 @@
+class RandomAgent():
+    def __init__(self):
+        pass
+
+    def observe(self, state, action, next_state, reward):
+        pass
+
+    def select_action(self, state):
+        pass
+
+    def update(self):
+        pass
+
+
+class DqnAgent():
+    def __init__(self):
+        pass
+
+    def observe(self, state, action, next_state, reward):
+        pass
+
+    def select_action(self, state):
+        pass
+
+    def update(self):
+        pass
+
+
+class DynaAgent():
+    def __init__(self):
+        pass
+
+    def observe(self, state, action, next_state, reward):
+        pass
+
+    def select_action(self, state):
+        pass
+
+    def update(self):
+        pass
