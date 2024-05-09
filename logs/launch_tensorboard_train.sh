39f47bac802e6d26d0b5cd5e0d7a51ef012cdc12
Alex <alexandre.hebert@epfl.ch>
2024-05-09 05:52:39 -0400

    Server lainch scripts for tensoboard

--- logs/launch_tensorboard_train.sh
+++ logs/launch_tensorboard_train.sh
@@ -1,7 +1,16 @@
-# random port
-port=$(((RANDOM%1000)+6006))
+# Check most recent dir
 logdir=$(ls -td ./train/* | head -n 1)
-# logdir="./logs/*"
-echo "Starting tensorboard: $logdir"
-open http://localhost:$port
-tensorboard --logdir=$logdir --port=$port
+# Check env var
+SERVER_MODE=${TENSORBOARD_SERVER:-0}
+if [ $SERVER_MODE -eq 1 ]; then
+    # fixed port
+    port=6123
+    echo "Starting tensorboard: $logdir"
+    tensorboard --logdir=$logdir --port=$port --bind_all
+else
+    # random port
+    port=$(((RANDOM%1000)+6006))
+    echo "Starting tensorboard: $logdir"
+    open http://localhost:$port
+    tensorboard --logdir=$logdir --port=$port
+fi
